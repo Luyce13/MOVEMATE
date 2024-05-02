53f8eb0af7a6579659caae8dfd22b3058d541ab4
RabyaMuhandis <154345078+RabyaMuhandis@users.noreply.github.com>
2024-05-02 22:40:33 +0500

    random

--- frontend/reuseableComponents/NavbarDetails.tsx
+++
@@ -1,8 +0,0 @@
-// columns.js
-export const columns = [
-    { Header: 'BusNo', accessor: 'BusNo' },
-    { Header: 'Route', accessor: 'Route' },
-    { Header: 'Time', accessor: 'Time' },
-   
-  ];
-  
